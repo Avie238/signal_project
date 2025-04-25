e23763a4783155d402ba30758d29f7d48ef1a8a5
Avie238 <ania.dymowska238@gmail.com>
2025-04-25 14:40:55 +0200

    Built jar file

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = [
+      "aarch64-linux"
+      "x86_64-linux"
+    ];
+
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs systems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forAllSystems ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          maven
+          zulu11
+        ];
+      };
+    });
+  };
+}
